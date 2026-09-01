9568548ef190a928ca59ee7db97cdd8428d04637
Daniel Price <dp@infoliage.com>
2026-09-01 13:58:32 -0700

    fix: add license info

--- abi.go
+++ abi.go
@@ -1,3 +1,8 @@
+// Copyright 2026 Infoliage LLC. All rights reserved.
+// Use is subject to license terms.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 package hayro
 
 // Low-level calls into a Document's wasm module instance — the only place
